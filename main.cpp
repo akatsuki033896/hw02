565d62c7eb77375dd37749595171b4f154770a18
akatsuki <akatsuki@akatsukideMacBook-Air.local>
2026-01-13 23:35:53 +0800

    hw02 initial submission

--- main.cpp
+++ main.cpp
@@ -4,51 +4,67 @@
 
 struct Node {
     // 这两个指针会造成什么问题？请修复
-    std::shared_ptr<Node> next;
-    std::shared_ptr<Node> prev;
     // 如果能改成 unique_ptr 就更好了!
-
+    // std::shared_ptr<Node> next;
+    // std::shared_ptr<Node> prev;
+    std::unique_ptr<Node> next;
+    Node* prev = nullptr;
     int value;
 
     // 这个构造函数有什么可以改进的？
-    Node(int val) {
-        value = val;
-    }
+    // Node(int val) {
+    //     value = val;
+    // }
+    explicit Node(int val) : value(val) {}
 
     void insert(int val) {
-        auto node = std::make_shared<Node>(val);
-        node->next = next;
-        node->prev = prev;
-        if (prev)
-            prev->next = node;
-        if (next)
-            next->prev = node;
+        auto node = std::make_unique<Node>(val);
+        // node->next = next;
+        node->next = std::move(this->next);
+        node->prev = this;
+        if (this->next) {
+            node->next->prev = node.get();
+        }
     }
 
     void erase() {
-        if (prev)
-            prev->next = next;
-        if (next)
-            next->prev = prev;
+        if (this->next)
+            // next->prev = prev;
+            this->next->prev = this->prev;
+        if (this->prev) {
+            // prev->next = next;
+            this->prev->next = std::move(next);
+        }
     }
 
     ~Node() {
-        printf("~Node()\n");   // 应输出多少次？为什么少了？
+        printf("~Node(%d)\n", this->value);   // 应输出多少次？为什么少了？
+        // 原来是 shared_ptr 循环引用了析构不掉
     }
 };
 
 struct List {
-    std::shared_ptr<Node> head;
+    std::unique_ptr<Node> head = nullptr;
 
     List() = default;
 
     List(List const &other) {
         printf("List 被拷贝！\n");
-        head = other.head;  // 这是浅拷贝！
+        // head = other.head;  // 这是浅拷贝！
         // 请实现拷贝构造函数为 **深拷贝**
+        this->head = std::make_unique<Node>(other.head->value); // 浅拷贝
+        Node* this_head = this->head.get();
+        Node* other_next = other.head->next.get();
+        while (other_next) {
+            this_head->next = std::make_unique<Node>(other_next->value);
+            this_head->next->prev = this_head;
+            this_head = this_head->next.get();
+            other_next = other_next->next.get();
+        }
     }
 
     List &operator=(List const &) = delete;  // 为什么删除拷贝赋值函数也不出错？
+    // unique_ptr 本身就没有拷贝构造和拷贝赋值，List可以视作unique_ptr类型变量的集合肯定删了没问题
 
     List(List &&) = default;
     List &operator=(List &&) = default;
@@ -59,16 +75,20 @@ struct List {
 
     int pop_front() {
         int ret = head->value;
-        head = head->next;
+        head = std::move(head->next);
+        if (head) {
+            head->prev = nullptr;
+        }
         return ret;
     }
 
     void push_front(int value) {
-        auto node = std::make_shared<Node>(value);
-        node->next = head;
+        auto node = std::make_unique<Node>(value);
+        node->next = std::move(head);
         if (head)
-            head->prev = node;
-        head = node;
+            head->prev = node.get();
+        // head = node;
+        head = std::move(node);
     }
 
     Node *at(size_t index) const {
@@ -80,7 +100,8 @@ struct List {
     }
 };
 
-void print(List lst) {  // 有什么值得改进的？
+// void print(List lst) {  // 有什么值得改进的？
+void print(List const& lst) { 
     printf("[");
     for (auto curr = lst.front(); curr; curr = curr->next.get()) {
         printf(" %d", curr->value);
@@ -101,6 +122,7 @@ int main() {
 
     print(a);   // [ 1 4 9 2 8 5 7 ]
 
+    // 未通过erase()测试。其他没问题
     a.at(2)->erase();
 
     print(a);   // [ 1 4 2 8 5 7 ]
